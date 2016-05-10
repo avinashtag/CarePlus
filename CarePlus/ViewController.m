350e907d3ca81e057bf6cd476340ee814add47db
Avinash Tag <avinash.tag@rohde-schwarz.com>
2016-05-10 14:43:27 +0530

    Initial Commit

---
+++ CarePlus/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CarePlus
+//
+//  Created by Avinash Tag on 10/05/16.
+//  Copyright © 2016 ZooZoo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ CarePlus/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CarePlus
+//
+//  Created by Avinash Tag on 10/05/16.
+//  Copyright © 2016 ZooZoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
